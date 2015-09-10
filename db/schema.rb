b74b11cd169c6a9623e8b7ff618d4888525a75d8
evorick <rickkadlac@gmail.com>
2015-09-10 08:58:37 -0500

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909134354) do
+ActiveRecord::Schema.define(version: 20150910135717) do
 
   create_table "user_tasks", force: :cascade do |t|
     t.string   "description"
     t.date     "due"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
