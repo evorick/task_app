8b513d85a795a9c7d30de7222bed1b7167b961fe
evorick <rickkadlac@gmail.com>
2015-09-12 13:27:36 -0500

    changed redirect to tasks after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,11 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     # Set the user_id in the session hash to this user's id
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to user_tasks_url
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
+    redirect_to root_url
   end
 end
